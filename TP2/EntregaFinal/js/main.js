02c6c5834bb6ee53b9ef46b93796f39f956ea3e3
Alfredo Bollati <bollatialfredo@gmail.com>
2017-09-04 07:44:04 -0300

    page layout
    Former-commit-id: 2989b78ce19f53f90af36fa83baba6797d783ead

--- TP2/EntregaFinal/js/main.js
+++ TP2/EntregaFinal/js/main.js
@@ -1,38 +1,37 @@
 var ctx = document.getElementById("canvas").getContext("2d");
 var canvas = ctx.canvas;
 canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
+canvas.height = 500;
 var dragging = false;
 var x = 0;
 var y = 0;
 var figura;
 
-function Circle(paramPosX, paramPosY, paramRadio, paramColor) {
+function Circle (paramPosX, paramPosY, paramRadio) {
   this.posX = paramPosX;
   this.posY = paramPosY;
   this.radio = paramRadio;
-  this.color = paramColor;
+  this.color = getTexture();
 }
 function Triangle() {
-  this.color;
+
 }
 
 Triangle.prototype.draw = function (aX, aY, bX, bY, cX, cY) {
-  console.log("triangulo");
   var path = new Path2D();
   path.moveTo(aX, aY);
   path.lineTo(bX, bY);
   path.lineTo(cX, cY);
-  ctx.fillStyle = "blue";
   ctx.fill(path);
 };
 
 Circle.prototype.draw = function (x, y) {
   this.posY = y;
   this.posX = x;
-  ctx.fillStyle = this.color;
   ctx.beginPath();
-  ctx.arc(x, y, this.radio, 0, 2*Math.PI);
+  ctx.arc(x, y, this.radio, 0, 2 * Math.PI);
+  var pat = ctx.createPattern(this.color,"no-repeat");
+  ctx.fillStyle = pat;
   ctx.fill();
   ctx.closePath();
 };
@@ -43,30 +42,41 @@ function clearBackground() {
 
 var circulo = new Circle(300, 300, 50, "#00FF00");
 var triangulo = new Triangle();
-triangulo.draw(100,100, 120, 80, 140, 100);
+// triangulo.draw(100,100, 120, 80, 140, 100);
 
 circulo.draw(100, 100);
 
 canvas.onmousedown = function(e){
-  console.log(e);
-  x = e.clientX;
-  y = e.clientY;
+  x = e.layerX;
+  y = e.layerY;
   var val = Math.sqrt(Math.pow((x - circulo.posX), 2) + Math.pow((y - circulo.posY), 2));
   if (val < circulo.radio){
     dragging = true;
   }
-}
+};
 
 canvas.onmousemove = function(e){
-  triangulo.draw(100,100, 120, 80, 140, 100);
+  // triangulo.draw(100,100, 120, 80, 140, 100);
   if(dragging){
-    x = e.x;
-    y = e.y;
+    x = e.layerX;
+    y = e.layerY;
     clearBackground();
     circulo.draw(x, y);
   }
-}
+};
 
 canvas.onmouseup = function(e){
   dragging = false;
+};
+
+function drawContext() {
+    ctx.drawImage(getTexture(), 0, 0);
+
+}
+
+function getTexture(){
+    var texture = new Image();
+    var num = Math.floor((Math.random() * 5) + 1);
+    texture.src = "assets/texture_" + num + ".jpg";
+    return texture;
 }
