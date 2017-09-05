9e2f259577f73eeb73cc2796df736bf98bba76a2
Alfredo <abollati@beerealit.com>
2017-09-05 10:57:23 -0300

    multiple objects drag and drop
    Former-commit-id: 2761d24b033fc813cd931c2199a8639ef8e42b89

--- TP2/EntregaFinal/js/main.js
+++ TP2/EntregaFinal/js/main.js
@@ -1,18 +1,12 @@
 var ctx = document.getElementById("canvas").getContext("2d");
 var canvas = ctx.canvas;
-canvas.width = document.body.clientWidth;
-canvas.height = 500;
+canvas.width = 555;
+canvas.height = window.innerHeight - 510;
 var dragging = false;
 var x = 0;
 var y = 0;
-var figura;
+var firstSelected = false;
 
-function Circle (paramPosX, paramPosY, paramRadio) {
-  this.posX = paramPosX;
-  this.posY = paramPosY;
-  this.radio = paramRadio;
-  this.color = getTexture();
-}
 function Triangle() {
 
 }
@@ -25,48 +19,97 @@ Triangle.prototype.draw = function (aX, aY, bX, bY, cX, cY) {
   ctx.fill(path);
 };
 
+var figurasArray = [];
+
+function Circle (paramPosX, paramPosY, paramRadio, color) {
+  this.posX = paramPosX;
+  this.posY = paramPosY;
+  this.radio = paramRadio;
+  this.selected = false;
+  this.color = color;
+}
+
 Circle.prototype.draw = function (x, y) {
   this.posY = y;
   this.posX = x;
   ctx.beginPath();
   ctx.arc(x, y, this.radio, 0, 2 * Math.PI);
-  var pat = ctx.createPattern(this.color,"no-repeat");
-  ctx.fillStyle = pat;
+  // var pat = ctx.createPattern(this.color,"no-repeat");
+  ctx.fillStyle = this.color;
   ctx.fill();
   ctx.closePath();
 };
+Circle.prototype.move = function(){
+  if(dragging){
+    clearBackground();
+    this.draw(x, y);
+  }
+};
+Circle.prototype.detectClickInside = function(){
+  var val = Math.sqrt(Math.pow((x - this.posX), 2) + Math.pow((y - this.posY), 2));
+  if (val < this.radio && !firstSelected){
+    dragging = true;
+    this.selected = true;
+    firstSelected = true;
+    console.log(figurasArray);
+    var aux = figurasArray[figurasArray.length-1];
+    var pos = figurasArray.indexOf(this);
+    figurasArray[figurasArray.length-1] = this;
+    figurasArray[pos] = aux;
+  }
+}
 
 function clearBackground() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+
 var circulo = new Circle(300, 300, 50, "#00FF00");
+var circulo2 = new Circle(100, 100, 40, "#0000FF");
+var circulo3 = new Circle(100, 100, 60, "#FF0000");
+figurasArray.push(circulo);
+figurasArray.push(circulo2);
+figurasArray.push(circulo3);
 var triangulo = new Triangle();
 // triangulo.draw(100,100, 120, 80, 140, 100);
+figurasArray.forEach(function(fig) {
+  fig.draw(fig.posX, fig.posY);  
+});
 
-circulo.draw(100, 100);
 
 canvas.onmousedown = function(e){
-  x = e.layerX;
-  y = e.layerY;
-  var val = Math.sqrt(Math.pow((x - circulo.posX), 2) + Math.pow((y - circulo.posY), 2));
-  if (val < circulo.radio){
-    dragging = true;
+  for (var i = figurasArray.length-1; i >= 0; i--) {
+    if (!firstSelected) {
+      figurasArray[i].detectClickInside();
+    }
   }
 };
 
 canvas.onmousemove = function(e){
   // triangulo.draw(100,100, 120, 80, 140, 100);
+  x = e.layerX;
+  y = e.layerY;
   if(dragging){
-    x = e.layerX;
-    y = e.layerY;
     clearBackground();
-    circulo.draw(x, y);
+    figurasArray.forEach(function(fig) {
+      if(fig.selected){
+        fig.draw(x, y);
+      }else{
+        fig.draw(fig.posX, fig.posY);
+      }
+    });
   }
 };
 
+
+
+
 canvas.onmouseup = function(e){
   dragging = false;
+  figurasArray.forEach(function(fig) {
+   fig.selected = false; 
+  });
+  firstSelected = false;
 };
 
 function drawContext() {
